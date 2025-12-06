271624731d01b0f3dad07095587b743147ef126f
Edo90 <edward.colon26@gmail.com>
2025-12-06 10:51:47 -0400

    edit page for employee

--- WebApp/Dtos/EmployeeDto.cs
+++ WebApp/Dtos/EmployeeDto.cs
@@ -1,11 +1,17 @@
-﻿namespace WebApp.Dtos
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApp.Dtos
 {
 	public class EmployeeDto
 	{
 		public int Id { get; set; }
+		[Required]
 		public string Name { get; set; } = string.Empty;
+		[Required]
 		public DateTime Birthdate { get; set; }
+		[Required]
 		public string IdentityNumber { get; set; } = string.Empty;
+		[EmailAddress]
 		public string? Email { get; set; }
 	}
 }
